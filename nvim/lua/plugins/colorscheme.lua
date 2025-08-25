24a3d312afb6e981d0bfd0189648dabbd1529740
Stiffjobs <charleschen0306@gmail.com>
2025-08-26 00:24:45 +0800

    update 0826

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,24 @@
 function ColorMyPencils(color)
   color = color or "rose-pine"
   vim.cmd.colorscheme(color)
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
   -- add dracula
   { "Mofiqul/dracula.nvim" },
+  {
+    "tiagovla/tokyodark.nvim",
+    opts = {
+      -- custom options here
+      transparent_background = true,
+    },
+    config = function(_, opts)
+      require("tokyodark").setup(opts) -- calling setup is optional
+      vim.cmd([[colorscheme tokyodark]])
+    end,
+  },
   {
     "craftzdog/solarized-osaka.nvim",
     lazy = false,
