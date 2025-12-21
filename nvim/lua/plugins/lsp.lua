94115e65e6805aa016842774efd9d253225c20d1
Stiffjobs <charleschen0306@gmail.com>
2025-12-21 17:51:01 +0900

    fix swift error

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -101,8 +101,6 @@ return {
         },
         sourcekit = {
           cmd = { "xcrun", "sourcekit-lsp" },
-          filetypes = { "swift", "objective-c", "objective-cpp" },
-          root_dir = require("lspconfig.util").root_pattern("Package.swift", ".git"),
         },
         gopls = {},
         elixirls = {},
